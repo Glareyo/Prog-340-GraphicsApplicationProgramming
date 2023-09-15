ae0087e3e16eb3d1ac78bdfd133c4fe7336a268f
Glareyo <47433630+Glareyo@users.noreply.github.com>
2023-09-14 19:04:27 -0500

    Add files via upload

--- Week 2_Intro to JS/main.js
+++ Week 2_Intro to JS/main.js
@@ -1,1 +1,7 @@
 document.getElementById('main-form');
+
+const btn = document.querySelector('.button');
+
+btn.addEventListener('click',(e) => {
+    //document.querySelector('#product-result'). = 'Hello';
+});
