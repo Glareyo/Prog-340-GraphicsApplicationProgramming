ba697226b975f3a84033cbdcb8c1fc70f6241cb5
Glareyo <47433630+Glareyo@users.noreply.github.com>
2023-10-05 19:04:25 -0500

    Added Files

---
+++ Wk5_NodeJS/Wk5_Demo/js/script.js
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+var height = window.innerHeight;
+var width = window.innerWidth;
+
+const renderer = new THREE.WebGL1Renderer();
+
+renderer.setSize(width,height);
+
+document.body.appendChild(renderer,domElement);
