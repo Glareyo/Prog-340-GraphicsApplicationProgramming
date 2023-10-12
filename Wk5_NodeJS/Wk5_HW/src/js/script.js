49115f4765c58c360270c3efa5d015b99e1023c1
Glareyo <47433630+Glareyo@users.noreply.github.com>
2023-10-12 09:53:59 -0500

    Added Movement

--- Wk5_NodeJS/Wk5_HW/src/js/script.js
+++ Wk5_NodeJS/Wk5_HW/src/js/script.js
@@ -1,3 +1,10 @@
+///Credits
+// - https://stackoverflow.com/questions/44463634/how-to-change-color-with-three-js#:~:text=THREE.Material%20does%20some%20magic%20when%20you%20provide%20certain,change%20it%20you%20need%20to%20do%20yourMaterial.color.setRGB%20%281%2C0%2C1%29
+// Explained how to change colors on a material
+
+// - https://www.bing.com/videos/riverview/relatedvideo?q=ThreeJS+import+dat.gui&mid=3EBB511F05CE1DEFAAA33EBB511F05CE1DEFAAA3&FORM=VIRE
+// Helped demonstrate how to use and add GUI
+
 ///Things to search online about
 // - NodeJS ==> Installation
 // - Adding Shadows
@@ -25,10 +32,10 @@
 import * as THREE from "three";
 
 // Reason for specifics ==> Import from a specifc module.
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import {dat} from "browserslist";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import {GUI} from 'dat.gui';
 
-// import { data } from "browserslist";
+
 
 var height = window.innerHeight;
 var width = window.innerWidth;
@@ -36,6 +43,10 @@ var width = window.innerWidth;
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor("#e5e5e5");
 renderer.setSize(width,height);
+renderer.shadowMap = true;
+// renderer.setClearColorHex( 0x000000, 1 );
+// renderer.setClearColorHex( 0xffffff, 0);
+
 
 document.body.appendChild(renderer.domElement);
 
@@ -53,54 +64,103 @@ scene.add(axesHelper);
 camera.position.set(-10,30,30);
 orbit.update();
 
-var light = new THREE.PointLight(0xFFFFFF,1,500);
-light.position.set(10,0,25);
-scene.add(light);
+// const myDirectionalLight = new THREE.DirectionalLight(0xFFFFFF,0.2);
+// myDirectionalLight.castShadow = true;
+// scene.add(myDirectionalLight);
+// myDirectionalLight.position.set(-20,10,20);
 
-// Create the geometry and material of the box
-const boxGeo = new THREE.BoxGeometry();
-const boxMat = new THREE.MeshBasicMaterial({color: 0x00FF00});
-const box = new THREE.Mesh(boxGeo,boxMat);
-scene.add(box); // Add to Scene 
+const mySpotLight = new THREE.SpotLight(0xFFFFFF,0.2);
+mySpotLight.castShadow = true;
+scene.add(mySpotLight);
+mySpotLight.position.set(-20,10,20);
+
+
+const dirSpotLightHelper = new THREE.DirectionalLightHelper(mySpotLight);
+scene.add(dirSpotLightHelper);
+
+// scene.add(dirLightHelper);
+
+// var light = new THREE.PointLight(0xFFFFFF,1,500);
+// light.position.set(10,10,25);
+// scene.add(light);
+
+// var ambientLight = new THREE.AmbientLight(0xFFFFFF,1,500);
+// scene.add(ambientLight);
+
+
+//Cone Geometry ==> Rotating
+var coneHeight = 5;
+const coneGeo = new THREE.ConeGeometry(1,coneHeight);
+const coneMat = new THREE.MeshBasicMaterial();
+
+https://stackoverflow.com/questions/44463634/how-to-change-color-with-three-js#:~:text=THREE.Material%20does%20some%20magic%20when%20you%20provide%20certain,change%20it%20you%20need%20to%20do%20yourMaterial.color.setRGB%20%281%2C0%2C1%29
+//Explained how to change colors on a material
+coneMat.color.setRGB(0.1,0.3,0.4);
+
+const cone = new THREE.Mesh(coneGeo,coneMat);
+cone.position.set(0,coneHeight/2,0);
+cone.castShadow = true; //Sphere can now cast a shadow
+
+scene.add(cone);
+
+
+
+//Cone Geometry ==> Rotating
+var ringHeight = 8;
+const ringGeo = new THREE.RingGeometry(4,ringHeight,50,4,4,5);
+const ringMat = new THREE.MeshBasicMaterial();
+
+https://stackoverflow.com/questions/44463634/how-to-change-color-with-three-js#:~:text=THREE.Material%20does%20some%20magic%20when%20you%20provide%20certain,change%20it%20you%20need%20to%20do%20yourMaterial.color.setRGB%20%281%2C0%2C1%29
+//Explained how to change colors on a material
+ringMat.color.setRGB(0.1,0.3,0.4);
+
+const ring = new THREE.Mesh(ringGeo,ringMat);
+ring.position.set(0,ringHeight,0);
+ring.castShadow = true; //Ring can now cast a shadow
+
+scene.add(ring);
 
 //Plane Object
-const planeGeo = new THREE.BoxGeometry(30,30);
-const planeMat = new THREE.MeshStandardMaterial({color: 0xFFFFFF, side:THREE.DoubleSide});
+const planeGeo = new THREE.BoxGeometry(30,30,0.4);
+const planeMat = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side:THREE.DoubleSide});
 const plane = new THREE.Mesh(planeGeo,planeMat);
+planeMat.color.setRGB(1,1,1);
+
+plane.position.set(0,0,0);
 plane.receiveShadow = true; //plane can now recieve a shadow ==> Shadow will appear on the plane
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
 
-// Sphere object
-const sphereGeo = new THREE.SphereGeometry(4,36,36); 
-const sphereMat = new THREE.MeshStandardMaterial({color: 0xFFFFFF,wireframe:false});
-const sphere = new THREE.Mesh(sphereGeo,sphereMat);
-sphere.position.set(-10,10,0);
 
-scene.add(sphere);
+//https://www.bing.com/videos/riverview/relatedvideo?q=ThreeJS+import+dat.gui&mid=3EBB511F05CE1DEFAAA33EBB511F05CE1DEFAAA3&FORM=VIRE
+//Helped demonstrate how to use and add GUI
+const gui = new GUI();
+gui.add(cone.position,"x",-10,10,0.1).name("Cone: X Position");
+gui.add(ring.rotation,"y",0,5,0.1).name("Ring: Y Rotation");
 
-
-//Current problem wit gui
-const gui = new dat.gui();
 const guiOptions = {
-    SphereColor: '#0000FF',
+    RingColor: '#0000FF',
+    RingSpeed: 1,
 };
-                        // SphereColor ==> must match name in the guiOptions
-gui.addColor(guiOptions,'SphereColor').onChange(function(e){sphere.material.color.set(e);});
+
+gui.addColor(guiOptions,'RingColor').onChange(function(e){ring.material.color.set(e);});
+
+var RingSpeed = 0.1;
 
 function animate(time)
 {
-    box.rotation.x = time / 1000;
-    box.rotation.y = time / 1000;
+    cone.rotation.x = time / 1000;
+    cone.rotation.z = time / 1000;
+    cone.rotation.y = time / 1000;
+
+
+    RingSpeed +=0.01;
+
+    ring.position.x = -10 * Math.sin(RingSpeed);
+    ring.rotation.z = -10 * Math.sin(RingSpeed);
+
     renderer.render(scene,camera);
 
 }
 
 renderer.setAnimationLoop(animate);
-// var render = function(){
-//     requestAnimationFrame(render);
-
-//     renderer.render(scene,camera);
-// }
-
-// render();
